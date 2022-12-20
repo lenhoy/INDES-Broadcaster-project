71e8a3548abf3e6ff5e9601b5ebbf147c0618bd1
Lennart Hoyer <lennart.hoyer@gmail.com>
2022-12-20 12:23:21 +0000

    Revert "Add name prop + naming function, add regions"
    This reverts commit 8ed531836fede55d82bb1e4028cfac4c153decbe. as it broke something

--- OBSUWP/DataClasses/Scene.cs
+++ OBSUWP/DataClasses/Scene.cs
@@ -7,29 +7,23 @@ namespace OBSUWP.DataClasses
 {
     public partial class Scene : ObservableObject
     {
-        #region Properties
         public Scene Self { get => this; }
         
         [NotifyPropertyChangedFor(nameof(Self))] // TODO find out how to make sure scene if deleted
         [ObservableProperty]
         private ObservableCollection<ISource> sources;
 
-        public string Name { get; private set; }
-        #endregion
 
-        #region Constructors
         public Scene()
         {
             sources = new ObservableCollection<ISource>();
-            SetName();
         }
         
         public Scene(Collection<ISource> sources)
         {
             Sources = (ObservableCollection<ISource>)sources;
-            SetName();
         }
-        #endregion
+
 
         #region Sources
         public void AddSource(ISource source)
@@ -48,15 +42,5 @@ namespace OBSUWP.DataClasses
             return ref sources;
         }
         #endregion Sources
-
-        #region Helpers
-        /// <summary>
-        /// Sets the name simply by current number of scenes
-        /// </summary>
-        private void SetName()
-        {
-            this.Name = "Scene " + MainPageViewModel.Current.Scenes.Count.ToString();
-        }
-        #endregion
     }
 }
